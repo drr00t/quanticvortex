0c5cfe48cb8c0cca3cf0b99b2852b2453881a5b9
Adriano Ribeiro <drr00t@users.noreply.github.com>
2010-02-09 16:23:15 -0200

    1 - include test framework on test project.

--- engine tests/source/main.cpp
+++ engine tests/source/main.cpp
@@ -1,6 +1,14 @@
 #include "UnitTest++.h"
 #include "TestReporterStdout.h"
 
+TEST( HelloUnitTestPP )
+
+{
+
+   CHECK( false );
+
+}
+
 int main(int, char const *[])
 {
     return UnitTest::RunAllTests();
