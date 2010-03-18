ff5127d6c618157e301d0cd69b595c88254043f2
Adriano Ribeiro <drr00t@users.noreply.github.com>
2010-03-18 17:42:20 -0300

    1 - external dependencies update

--- engine samples/01_Sample/source/main.cpp
+++ engine samples/01_Sample/source/main.cpp
@@ -2,13 +2,25 @@
 //#include "game/Game.h"
 #include "sdcSylfurDCGame.h"
 
+typedef struct teste
+{
+    int ind;
+    char c;
+}STeste;
+
 s32 main(s32 argc, c8 *argv[])
 {
+    STeste* t = new STeste();
+
+    t->c = 'e';
+    t->ind = 10;
+
     sdc::SylfurDCGame game;
-    
+
 	if(game.initialize())
 		return game.run( argc, argv); //game.main(argc,argv);
-	return -1;
+
+    delete t;
 }
 
 
