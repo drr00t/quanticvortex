3ed013463e28b3b3ae9f7fe2b196b02f2936e57e
Adriano Ribeiro <drr00t@users.noreply.github.com>
2010-04-05 22:15:53 -0300

    1 - file that was missing

---
+++ engine/include/qvTypes.h
@@ -0,0 +1,59 @@
+/**************************************************************************************************
+
+//This code is part of QuanticVortex for latest information, see http://www.quanticvortex.org
+//
+//Copyright (c) 2009-2010 QuanticMinds Software Ltda.
+//
+//Permission is hereby granted, free of charge, to any person obtaining a copy
+//of this software and associated documentation files (the "Software"), to deal
+//in the Software without restriction, including without limitation the rights
+//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included in
+//all copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//THE SOFTWARE.
+
+**************************************************************************************************/
+
+
+#ifndef __QV_TYPES_H_
+#define __QV_TYPES_H_
+
+#include <inttypes.h>
+#include <string>
+
+namespace qv
+{
+
+    typedef uint8_t	    u8;
+
+    typedef int8_t		s8;
+
+    typedef uint16_t	u16;
+
+    typedef int16_t	    s16;
+
+    typedef uint32_t	u32;
+
+    typedef int32_t		s32;
+
+
+    typedef char		c8;
+
+    typedef std::char_traits<u8> uchar_traits;
+    typedef std::basic_string<u8, uchar_traits> ustring;
+
+    typedef std::char_traits<c8> char_traits;
+    typedef std::basic_string<c8, char_traits> string;
+}
+
+#endif
